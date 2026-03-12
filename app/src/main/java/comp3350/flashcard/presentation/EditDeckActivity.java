c17fe22e3bceeb7fb1461e4d0d500a743a054212
Nur Hossain Arman <armannh@myumanitoba.ca>
2026-03-12 00:19:10 -0500

    Removed redundant comment (i1 feedback)

--- app/src/main/java/comp3350/flashcard/presentation/EditDeckActivity.java
+++ app/src/main/java/comp3350/flashcard/presentation/EditDeckActivity.java
@@ -37,9 +37,9 @@ public class EditDeckActivity extends AppCompatActivity {
      * Finds the input boxes and buttons on the screen.
      */
     private void initUI() {
-        inputDeckName = findViewById(R.id.inputDeckName); //Find input field for deck name
-        Button btnSaveDeck = findViewById(R.id.btnSaveDeck); //Find save button
-        Toolbar toolbar = findViewById(R.id.toolbar); //Find toolbar(containing deck name)
+        inputDeckName = findViewById(R.id.inputDeckName); 
+        Button btnSaveDeck = findViewById(R.id.btnSaveDeck);
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
         // If intent returns a deck ID, we are editing an existing deck
         // Otherwise, we are creating a new deck (deckId == -1)
